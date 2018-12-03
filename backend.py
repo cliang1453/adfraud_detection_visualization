e3bcf6f8d1a551a66d2ddb4cff25c125a398cc93
杨博文 <yangbowen@lawn-143-215-92-5.lawn.gatech.edu>
2018-12-03 13:56:21 -0500

    backend commit

---
+++ backend.py
@@ -0,0 +1,16 @@
+from run import *
+from flask import Flask, request, json
+app = Flask(__name__)
+@app.route('/result',methods = ['POST', 'GET'])
+def result():
+    if request.method == 'GET':
+        attribute = []
+        #convert string to int
+        attribute.append(int(request.form['ip']))
+        attribute.append(int(request.form['app']))
+        attribute.append(int(request.form['os']))
+        attribute.append(int(request.form['device']))
+        attribute.append(int(request.form['channel']))
+        attribute.append(int(request.form['click_time']))
+        predict_result = model.predict(attribute)
+        return render_template("index.html", label = predict_result)
